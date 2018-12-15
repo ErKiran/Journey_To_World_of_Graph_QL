a55a8bbcc03e0fdecbc6e8cacf5f64bf015f151e
Kiran <kiruu1238@gmail.com>
2018-12-15 17:39:39 +0545

    Custom Datatypes Created

---
+++ customdatatypes.js
@@ -0,0 +1,78 @@
+import { GraphQLServer } from 'graphql-yoga';
+import axios from 'axios';
+const number = 1;
+const details = {
+    id: '',
+    email: '',
+    username: '',
+    age: ''
+}
+const locations = {
+    city: '',
+    postcode: ''
+}
+
+axios.get(`https://randomuser.me/api/?results=${number}`)
+    .then(res => {
+        details.id = res.data.results[0].login.uuid;
+        details.email = res.data.results[0].email;
+        details.username = res.data.results[0].login.username;
+        details.age = res.data.results[0].dob.age;
+        locations.city = res.data.results[0].location.city;
+        locations.postcode = res.data.results[0].location.postcode;
+        return `
+        ${details.id},
+        ${details.email},
+        ${details.username},
+        ${details.age},
+        ${locations.city},
+        ${locations.postcode}
+        `
+    });
+
+// Type Definations
+const typeDefs = `
+type Query{
+    me: User!
+    address: Location!
+}
+type User {
+    id: ID!
+    email: String!
+    age: Int!
+    username: String!
+}
+type Location {
+    city: String!
+    postcode: Int!
+}
+`
+// Resolvers
+
+const resolvers = {
+    Query: {
+        me() {
+            return {
+                id: details.id,
+                email: details.email,
+                username: details.username,
+                age: details.age
+            }
+        },
+        address() {
+            return {
+                city: locations.city,
+                postcode: locations.postcode
+            }
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('Hurray! Server is up and running')
+});
