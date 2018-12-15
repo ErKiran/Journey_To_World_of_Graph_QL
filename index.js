c705b1c674440a3faf445e032c6a0696cb81f5db
Kiran <kiruu1238@gmail.com>
2018-12-15 12:50:33 +0545

    Random User API integrated for the GraphQL Query

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 import { GraphQLServer } from 'graphql-yoga';
-const name = {
-    first: 'Kiran',
-    last: 'Adhikari'
+import axios from 'axios';
+const number = 1;
+const details = {
+    first: '',
+    last: '',
+    location: ''
 }
+
+const result = axios.get(`https://randomuser.me/api/?results=${number}`)
+    .then(res => {
+        details.first = res.data.results[0].name.first;
+        details.last = res.data.results[0].name.last;
+        details.city = res.data.results[0].location.city;
+        return `${details.first}, ${details.last}, ${details.city}`;
+    });
 
 // Type Definations
 const typeDefs = `
 type Query{
     hello: String!
     name: String!
+    city: String!
 }
 `
 // Resolvers
@@ -19,7 +31,10 @@ const resolvers = {
             return 'This is my first query in Graph-QL yoga'
         },
         name() {
-            return `My name is ${name.first} ${name.last}`
+            return `My name is ${details.first} ${details.last}`
+        },
+        city() {
+            return `I live in ${details.city}`
         }
     }
 }
